838053678fa8ffb8832ab46d7893352a3212288b
Brian Provost <brianrp@icloud.com>
2025-12-23 14:50:29 -0500

    feat: working UART read and write

--- src/main.c
+++ src/main.c
@@ -1,51 +1,11 @@
 //*****************************************************************************
 //
-//! @file hello_world_uart.c
+//! @file main.c
 //!
-//! @brief A simple "Hello World" example using the UART peripheral.
+//! @brief A simple read and write example using UART
 //!
-//! Purpose: This example prints a "Hello World" message with some device info
-//! over UART at 115200 baud. To see the output of this program, run AMFlash,
-//! and configure the console for UART. The example sleeps after it is done
-//! printing.
-//
-//*****************************************************************************
-
-//*****************************************************************************
-//
-// Copyright (c) 2019, Ambiq Micro
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice,
-// this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the copyright holder nor the names of its
-// contributors may be used to endorse or promote products derived from this
-// software without specific prior written permission.
-//
-// Third party software included in this distribution is subject to the
-// additional license terms as defined in the /docs/licenses directory.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//
-// This is part of revision v2.2.0-7-g63f7c2ba1 of the AmbiqSuite Development Package.
+//! Purpose: Prints device info over UART. Then accepts 'y' or 'n' commands
+//  via UART to turn on or off LED.
 //
 //*****************************************************************************
 
@@ -53,105 +13,78 @@
 #include "am_mcu_apollo.h"
 #include "am_util.h"
 
-//*****************************************************************************
-//
-// UART handle.
-//
-//*****************************************************************************
+// UART handle
 void *phUART;
 
 #define CHECK_ERRORS(x)                 \
     if ((x) != AM_HAL_STATUS_SUCCESS) { \
         error_handler(x);               \
     }
-#define BLINK_PERIOD 100
+#define BLINK_PERIOD 500
 
 volatile uint32_t ui32LastError;
+volatile uint32_t g_rxIndex = 0;
+volatile bool g_led_state = false;
 
-//*****************************************************************************
-//
-// Catch HAL errors.
-//
-//*****************************************************************************
+// Catch HAL errors
 void error_handler(uint32_t ui32ErrorStatus) {
     ui32LastError = ui32ErrorStatus;
 
     while (1);
 }
 
-//*****************************************************************************
-//
-// UART buffers.
-//
-//*****************************************************************************
+// UART buffers
 uint8_t g_pui8TxBuffer[256];
-uint8_t g_pui8RxBuffer[2];
+uint8_t g_pui8RxBuffer[1];
 
-//*****************************************************************************
-//
-// UART configuration.
-//
-//*****************************************************************************
+// UART configuration
 const am_hal_uart_config_t g_sUartConfig =
     {
-        //
         // Standard UART settings: 115200-8-N-1
-        //
         .ui32BaudRate = 115200,
         .ui32DataBits = AM_HAL_UART_DATA_BITS_8,
         .ui32Parity = AM_HAL_UART_PARITY_NONE,
         .ui32StopBits = AM_HAL_UART_ONE_STOP_BIT,
         .ui32FlowControl = AM_HAL_UART_FLOW_CTRL_NONE,
-
-        //
-        // Set TX and RX FIFOs to interrupt at half-full.
-        //
+        // Set TX and RX FIFOs to interrupt at half-full
         .ui32FifoLevels = (AM_HAL_UART_TX_FIFO_1_2 |
                            AM_HAL_UART_RX_FIFO_1_2),
-
-        //
         // Buffers
-        //
         .pui8TxBuffer = g_pui8TxBuffer,
         .ui32TxBufferSize = sizeof(g_pui8TxBuffer),
         .pui8RxBuffer = g_pui8RxBuffer,
         .ui32RxBufferSize = sizeof(g_pui8RxBuffer),
 };
 
-//*****************************************************************************
-//
-// UART0 interrupt handler.
-//
-//*****************************************************************************
-void am_uart_isr(void) {
-    //
-    // Service the FIFOs as necessary, and clear the interrupts.
-    //
-    uint32_t ui32Status, ui32Idle;
-    am_hal_uart_interrupt_status_get(phUART, &ui32Status, true);
-    am_hal_uart_interrupt_clear(phUART, ui32Status);
-    am_hal_uart_interrupt_service(phUART, ui32Status, &ui32Idle);
+void board_init(void) {
+    // Set the clock frequency
+    am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
+
+    // Set the default cache configuration
+    am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+    am_hal_cachectrl_enable();
+
+    // Configure the board for low power operation
+    am_bsp_low_power_init();
 }
 
-//*****************************************************************************
-//
-// UART print string
-//
-//*****************************************************************************
+void led_init(void) {
+    uint32_t ui32GPIONumber = am_bsp_psLEDs[0].ui32GPIONumber;
+    am_hal_gpio_pinconfig(ui32GPIONumber, g_AM_HAL_GPIO_OUTPUT);
+    am_devices_led_off(am_bsp_psLEDs, 0);
+    g_led_state = false;
+}
+
 void uart_print(char *pcStr) {
     uint32_t ui32StrLen = 0;
     uint32_t ui32BytesWritten = 0;
 
-    //
-    // Measure the length of the string.
-    //
+    // Measure the length of the string
     while (pcStr[ui32StrLen] != 0) {
         ui32StrLen++;
     }
 
-    //
-    // Print the string via the UART.
-    //
+    // Print the string via the UART
     const am_hal_uart_transfer_t sUartWrite =
         {
             .ui32Direction = AM_HAL_UART_WRITE,
@@ -164,71 +97,91 @@ void uart_print(char *pcStr) {
     CHECK_ERRORS(am_hal_uart_transfer(phUART, &sUartWrite));
 
     if (ui32BytesWritten != ui32StrLen) {
-        //
-        // Couldn't send the whole string!!
-        //
+        // Couldn't send the whole string!
         while (1);
     }
 }
 
-//*****************************************************************************
-//
-// Main
-//
-//*****************************************************************************
-int main(void) {
-    am_util_id_t sIdDevice;
-    uint32_t ui32StrBuf;
-
-    //
-    // Set the clock frequency.
-    //
-    am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
-
-    //
-    // Set the default cache configuration
-    //
-    am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
-    am_hal_cachectrl_enable();
-
-    //
-    // Configure the board for low power operation.
-    //
-    am_bsp_low_power_init();
-
-    //
+void uart_init(void *uart_print) {
     // Initialize the printf interface for UART output.
-    //
     CHECK_ERRORS(am_hal_uart_initialize(0, &phUART));
     CHECK_ERRORS(am_hal_uart_power_control(phUART, AM_HAL_SYSCTRL_WAKE, false));
     CHECK_ERRORS(am_hal_uart_configure(phUART, &g_sUartConfig));
 
-    //
-    // Enable the UART pins.
-    //
+    // Enable the UART pins
     am_hal_gpio_pinconfig(AM_BSP_GPIO_COM_UART_TX, g_AM_BSP_GPIO_COM_UART_TX);
     am_hal_gpio_pinconfig(AM_BSP_GPIO_COM_UART_RX, g_AM_BSP_GPIO_COM_UART_RX);
 
-    //
-    // Enable interrupts.
-    //
+    // Enable interrupts
     NVIC_EnableIRQ((IRQn_Type)(UART0_IRQn + AM_BSP_UART_PRINT_INST));
     am_hal_interrupt_master_enable();
 
-    //
-    // Set the main print interface to use the UART print function we defined.
-    //
+    // Set the main print interface to use the UART print function we defined
     am_util_stdio_printf_init(uart_print);
+}
 
-    //
-    // Print the banner.
-    //
+// UART read/write ISR
+void am_uart_isr(void) {
+    uint32_t ui32Status;
+    am_hal_uart_interrupt_status_get(phUART, &ui32Status, true);
+    am_hal_uart_interrupt_clear(phUART, ui32Status);
+    am_hal_uart_interrupt_service(phUART, ui32Status, NULL);
+
+    // Read available data into your buffer
+    if (ui32Status & (AM_HAL_UART_INT_RX | AM_HAL_UART_INT_RX_TMOUT)) {
+        uint32_t bytesRead;
+        am_hal_uart_transfer_t sRead = {
+            .ui32Direction = AM_HAL_UART_READ,
+            .pui8Data = &g_pui8RxBuffer[g_rxIndex],
+            .ui32NumBytes = sizeof(g_pui8RxBuffer) - g_rxIndex,
+            .ui32TimeoutMs = 0,
+            .pui32BytesTransferred = &bytesRead,
+        };
+        am_hal_uart_transfer(phUART, &sRead);
+        g_rxIndex += bytesRead;
+    }
+}
+
+void toggle_led(void) {
+    g_led_state = !g_led_state;
+    (g_led_state) ? am_devices_led_on(am_bsp_psLEDs, 0) : am_devices_led_off(am_bsp_psLEDs, 0);
+}
+
+void led_on(void) {
+    am_devices_led_on(am_bsp_psLEDs, 0);
+}
+
+void led_off(void) {
+    am_devices_led_off(am_bsp_psLEDs, 0);
+}
+
+// Process UART read buffer
+void process_command(uint8_t *pcStr, uint32_t len) {
+    // len is number of bytes read
+    switch (pcStr[0]) {
+        case 'y':
+            led_on();
+            am_util_stdio_printf("Turning LED on\n");
+            break;
+        case 'n':
+            led_off();
+            am_util_stdio_printf("Turning LED off\n");
+            break;
+        default:
+            am_util_stdio_printf("Unrecognized command. Enter 'y' to turn on LED, 'n' to turn off LED.\n");
+            break;
+    }
+}
+
+void print_device_data(void) {
+    am_util_id_t sIdDevice;
+    uint32_t ui32StrBuf;
+
+    // Print the banner
     am_util_stdio_terminal_clear();
     am_util_stdio_printf("Hello World!\n\n");
 
-    //
-    // Print the device info.
-    //
+    // Print the device info
     am_util_id_device(&sIdDevice);
     am_util_stdio_printf("Vendor Name: %s\n", sIdDevice.pui8VendorName);
     am_util_stdio_printf("Device type: %s\n", sIdDevice.pui8DeviceName);
@@ -248,9 +201,7 @@ int main(void) {
         sIdDevice.sMcuCtrlDevice.ui32ChipRev,
         sIdDevice.ui8ChipRevMaj, sIdDevice.ui8ChipRevMin);
 
-    //
-    // If not a multiple of 1024 bytes, append a plus sign to the KB.
-    //
+    // If not a multiple of 1024 bytes, append a plus sign to the KB
     ui32StrBuf = (sIdDevice.sMcuCtrlDevice.ui32FlashSize % 1024) ? '+' : 0;
     am_util_stdio_printf("\tFlash size:  %7d (%d KB%s)\n",
                          sIdDevice.sMcuCtrlDevice.ui32FlashSize,
@@ -263,12 +214,9 @@ int main(void) {
                          sIdDevice.sMcuCtrlDevice.ui32SRAMSize / 1024,
                          &ui32StrBuf);
 
-    //
-    // Print the compiler version.
-    //
+    // Print the compiler version
     am_hal_uart_tx_flush(phUART);
     am_util_stdio_printf("App Compiler:    %s\n", COMPILER_VERSION);
-#ifdef AM_PART_APOLLO3
     am_util_stdio_printf("HAL Compiler:    %s\n", g_ui8HALcompiler);
     am_util_stdio_printf("HAL SDK version: %d.%d.%d\n",
                          g_ui32HALversion.s.Major,
@@ -293,28 +241,26 @@ int main(void) {
     } else {
         am_util_stdio_printf("am_hal_security_get_info failed 0x%X\n", ui32Status);
     }
-#endif  // AM_PART_APOLLO3
+}
 
-    //
-    // We are done printing.
-    // Disable the UART and interrupts
-    //
-    am_hal_uart_tx_flush(phUART);
-    CHECK_ERRORS(am_hal_uart_power_control(phUART, AM_HAL_SYSCTRL_DEEPSLEEP, false));
+//*****************************************************************************
+//
+// Main
+//
+//*****************************************************************************
+int main(void) {
+    board_init();
+    uart_init(uart_print);
+    led_init();
 
-    // Set up LED
-    uint32_t ui32GPIONumber = am_bsp_psLEDs[0].ui32GPIONumber;
-    am_hal_gpio_pinconfig(ui32GPIONumber, g_AM_HAL_GPIO_OUTPUT);
-    am_devices_led_off(am_bsp_psLEDs, 0);
-    bool led_state = false;
+    print_device_data();
 
-    //
-    // Loop forever
-    //
     while (1) {
-        led_state = !led_state;
-        ui32GPIONumber = am_bsp_psLEDs[0].ui32GPIONumber;
-        (led_state) ? am_devices_led_on(am_bsp_psLEDs, 0) : am_devices_led_off(am_bsp_psLEDs, 0);
-        am_util_delay_ms(BLINK_PERIOD);
+        // Read data from UART
+        if (g_rxIndex > 0) {
+            // Process received data...
+            process_command(g_pui8RxBuffer, g_rxIndex);
+            g_rxIndex = 0;
+        }
     }
 }

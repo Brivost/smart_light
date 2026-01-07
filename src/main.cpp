ddc4c985453d96ce3bd38e91c2c28ec0158f6c56
Brian Provost <brianrp@icloud.com>
2026-01-07 15:24:50 -0500

    feat: working linear layer

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "am_bsp.h"
 #include "am_mcu_apollo.h"
 #include "am_util.h"
+#include "arm_nnfunctions.h"
 #include "uart.h"
 
 void board_init() {
@@ -31,31 +32,36 @@ void led_off() {
     am_devices_led_off(am_bsp_psLEDs, 0);
 }
 
-void process_command(const uint8_t data) {
-    switch (data) {
-        case 'y':
-            led_on();
-            Serial.printf("Turning LED on\n");
-            break;
-        case 'n':
-            led_off();
-            Serial.printf("Turning LED off\n");
-            break;
-        default:
-            Serial.printf("Unrecognized command %d. Enter 'y' or 'n'.\n", data);
-            break;
-    }
-}
-
 int main() {
     board_init();
-
     Serial.begin(115200);
 
-    Serial.printf("\nEnter 'y' to turn LED on, 'n' to turn LED off.\n");
+    constexpr int32_t input_nodes = 3;
+    constexpr int32_t output_nodes = 2;
+
+    const int8_t input[] = {60, 74, 38};
+    const int8_t kernel[] = {-1, 83, -127, -114, -60, 42};
+    const int32_t bias[] = {0, 0};
+    int8_t output[output_nodes] = {};
+
+    void *ctx_buff;
+    const cmsis_nn_context ctx{.buf = ctx_buff, .size = 0};
+    cmsis_nn_activation activation{.min = -128, .max = 127};
+    const cmsis_nn_fc_params fc_params{.input_offset = 0, .filter_offset = 0, .output_offset = 0, activation = activation};
+    const cmsis_nn_per_tensor_quant_params quant_params{.multiplier = 13372818, .shift = 0};
+    const cmsis_nn_dims input_dims{.n = 1};
+    const cmsis_nn_dims filter_dims{.n = input_nodes, .c = output_nodes};
+    const cmsis_nn_dims bias_dims{.c = output_nodes};
+    const cmsis_nn_dims output_dims{.c = output_nodes};
+
+    arm_fully_connected_s8(&ctx, &fc_params, &quant_params, &input_dims, input, &filter_dims, kernel, &bias_dims, bias, &output_dims, output);
+
+    Serial.printf("\n");
+    for (int i = 0; i < output_nodes; ++i) {
+        Serial.printf("%d, ", output[i]);
+    }
+    Serial.printf("\n");
+
     while (1) {
-        if (Serial.available()) {
-            process_command(Serial.read());
-        }
     }
 }
